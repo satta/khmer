98107200cb562fc09c0003aaa89b9513cc005303
Daniel Standage <daniel.standage@gmail.com>
2016-08-11 00:47:25 -0700

    Improved C++ implementation

--- sandbox/snp-power.cpp
+++ sandbox/snp-power.cpp
@@ -1,6 +1,10 @@
+// g++ --std=c++11 -I ../lib -I ../third-party/seqan/core/include/ -o snp-power snp-power.cpp ../lib/liboxli.a
+
 #include <assert.h>
+#include <getopt.h>
 #include <cmath>
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -14,6 +18,42 @@ using namespace read_parsers;
 
 typedef std::vector<HashIntoType> vint;
 typedef std::vector<std::string> vstring;
+typedef Read Sequence;
+
+class Mutator
+{
+public:
+    Sequence& seq;
+    int k;
+    int i;
+    std::string nucl;
+
+    Mutator(Sequence& sequence, int ksize)
+        : seq(sequence), k(ksize), i(k - 1), nucl("ACGT") {}
+
+    bool next_mutated_seqs(vstring& mutseqs)
+    {
+        mutseqs.clear();
+        if (i + k > seq.sequence.length()) {
+            return false;
+        }
+
+        int minpos = i - k + 1;
+        int len = (2*k) - 1;
+        std::string window = seq.sequence.substr(minpos, len);
+
+        for (auto bp : nucl) {
+            if (window[k - 1] != bp) {
+                std::string mutseq = window;
+                mutseq[k - 1] = bp;
+                mutseqs.push_back(mutseq);
+            }
+        }
+
+        i++;
+        return true;
+    }
+};
 
 bool isprime(int n)
 {
@@ -57,55 +97,13 @@ vint get_n_primes_near_x(int x, int n)
     return primes;
 }
 
-void get_windows(std::string& sequence, int k, vstring& windows) {
-    for (unsigned long i = 0; i < sequence.length(); i++) {
-        if (i < k - 1 || i + k > sequence.length()) {
-            continue;
-        }
-        int minpos = i - k + 1;
-        int len = (2*k) - 1;
-        std::string window = sequence.substr(minpos, len);
-        windows.push_back(window);
-    }
-}
-
-void get_mutations(Read& seq, int k, unsigned long limit, vstring& mutseqs)
+void count_mutation_collisions(Hashbits& nodegraph, IParser *parser, int k,
+                               unsigned long limit)
 {
-    int i = k - 1;
-    std::string nucleotides("ACGT");
-    vstring windows;
-    get_windows(seq.sequence, k, windows);
-
-    int counter = 0;
-    vstring::iterator it;
-    for (it = windows.begin(); it < windows.end(); it++) {
-        counter++;
-        std::string window = *it;
-        assert(window.length() == (2*k) - 1);
-
-        if (counter > 0 && counter % 10000 == 0) {
-            std::cerr << "Processed " << counter / 1000 << " Kb of sequence '"
-                      << seq.name << "'" << std::endl;
-        }
-        if (limit > 0 && counter >= limit) {
-            break;
-        }
-
-        for (auto nucl : nucleotides) {
-            if (window[i] != nucl) {
-                std::string mutseq = window;
-                mutseq[i] = nucl;
-                mutseqs.push_back(mutseq);
-            }
-        }
-    }
-}
-
-void count_mutation_collisions(Hashbits& nodegraph, IParser *parser, int k, unsigned long limit)
-{
-    unsigned long total = 0;
+    unsigned long long total = 0;
     unsigned long hits = 0;
-    Read seq;
+    Sequence seq;
+    unsigned long count = 0;
 
     while (!parser->is_complete()) {
         try {
@@ -114,48 +112,98 @@ void count_mutation_collisions(Hashbits& nodegraph, IParser *parser, int k, unsi
             break;
         }
 
+        Mutator m(seq, k);
         vstring mutseqs;
-        get_mutations(seq, k, limit, mutseqs);
-        vstring::iterator it;
-        for (it = mutseqs.begin(); it < mutseqs.end(); it++) {
-            std::string mutseq = *it;
-            vstring kmers;
-            nodegraph.get_kmers(mutseq, kmers);
-            vstring::iterator kit;
-            for (kit = kmers.begin(); kit < kmers.end(); kit++) {
-                std::string kmer = *kit;
-                total++;
-                if (nodegraph.get_count(kmer.c_str()) > 0) {
-                    hits++;
+        while (m.next_mutated_seqs(mutseqs) && (limit == 0 || count < limit)) {
+            count++;
+            if (count % 100000 == 0) {
+                std::cerr << "  processed " << (float)count / (float)1000
+                          << " Kb of sequence" << std::endl;
+            }
+            vstring::iterator it;
+            for (it = mutseqs.begin(); it < mutseqs.end(); it++) {
+                std::string mutseq = *it;
+                vstring kmers;
+                nodegraph.get_kmers(mutseq, kmers);
+                vstring::iterator kit;
+                for (kit = kmers.begin(); kit < kmers.end(); kit++) {
+                    std::string kmer = *kit;
+                    total++;
+                    if (nodegraph.get_count(kmer.c_str()) > 0) {
+                        hits++;
+                    }
                 }
             }
         }
     }
-    std::cout << total << " " << hits << " " << (float)hits / (float)total << std::endl;
+    std::cout << total << " " << hits << " " << (float)hits / (float)total
+              << std::endl;
 }
 
-int main(int arg, const char **argv)
+void print_usage(std::ostream& stream = std::cerr)
+{
+    stream << "Usage: snp-power [options] genome.fa" << std::endl;
+    stream << "  options:" << std::endl;
+    stream << "    -h    print this help message and exit" << std::endl;
+    stream << "    -k    k-mer length (default: 31)" << std::endl;
+    stream << "    -x    approx table size (default: 5e8)" << std::endl;
+    stream << "    -N    num tables (default: 4)" << std::endl;
+    stream << "    -l    limit number of positions to process" << std::endl;
+}
+
+int main(int argc, const char **argv)
 {
     assert(isprime(37));
     assert(!isprime(51));
     assert(isprime(15485863));
     assert(!isprime(15485865));
 
+    if (argc == 1) {
+        print_usage(std::cout);
+        return 0;
+    }
+
     int k = 31;
     int targetsize = 5e8;
     int numtables = 4;
-    unsigned long limit = 10000;
-    std::string infile(argv[1]);
+    unsigned long limit = 0;
+
+    char c;
+    while ((c = getopt (argc, (char *const *)argv, "hk:x:N:l:")) != -1) {
+        if (c == 'h') {
+            print_usage(std::cout);
+            return 0;
+        }
+        else if (c == 'k') {
+            k = atoi(optarg);
+        }
+        else if (c == 'x') {
+            targetsize = atoi(optarg);
+        }
+        else if (c == 'N') {
+            numtables = atoi(optarg);
+        }
+        else if (c == 'l') {
+            limit = atoi(optarg);
+        }
+        else {
+            std::cerr << "Unknown option '" << c << "'" << std::endl;
+            print_usage();
+            return 1;
+        }
+    }
+
+    std::string infile(argv[optind]);
 
     std::cerr << "allocating nodegraph" << std::endl;
     vint tablesizes = get_n_primes_near_x(targetsize, numtables);
     Hashbits nodegraph(k, tablesizes);
 
     std::cerr << "consuming input" << std::endl;
-    unsigned int reads_consumed = 0;
+    unsigned int seqs_consumed = 0;
     unsigned long long kmers_consumed = 0;
-    nodegraph.consume_fasta(infile, reads_consumed, kmers_consumed);
-    std::cerr << "consumed " << reads_consumed << " reads and "
+    nodegraph.consume_fasta(infile, seqs_consumed, kmers_consumed);
+    std::cerr << "consumed " << seqs_consumed << " sequence(s) and "
               << kmers_consumed << " " << k << "-mers" << std::endl;
 
     std::cerr << "generating mutations" << std::endl;
